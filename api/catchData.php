5c58fabb4bc7e49a0c3ca7caf5c793f46a90db16
jhrkac <hrkacjosip0@gmail.com>
2023-05-09 14:16:49 +0200

    Successfully forward weather data from PHP script to JS script and display it on the browser. Handle errors and display appropriate messages.

--- api/catchData.php
+++ api/catchData.php
@@ -5,7 +5,7 @@ $apiKey = $config['api-key'];
 
 $city = $_GET['city'];
 
-$api_endpoint = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=$apiKey";
+$api_endpoint = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey";
 
 $curl = curl_init();
 
@@ -25,15 +25,17 @@ print_r($data); */
 
 if ($data['cod'] === 200) {
     $weatherData = array(
-        'date' => date('d M', $data['dt']),
+        'date' => date('Y-m-d H:i:s', $data['dt']),
         'temp' => round($data['main']['temp'] - 273.15),
         'description' => $data['weather']['0']['description']
     );
     $json_result = json_encode($weatherData);
     echo $json_result;
 } else {
+    http_response_code(404);
     $error = array(
-        'error' => 'An error occurred while fetching data'
+        'error' => 'An error occurred while fetching data.
+         Check the name of the city.'
     );
     $json_error = json_encode($error);
     echo $json_error;
--- script.js
+++ script.js
@@ -4,5 +4,17 @@ var weatherData = document.getElementById("container");
 
 btn.addEventListener("click", function () {
   var city = input.value;
-  fetch(`api/catchData.php?city=${city}`).then((response) => response.json);
+  fetch(`api/catchData.php?city=${city}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        weatherData.innerHTML = `<p>Error: ${data.error}</p>`;
+      } else
+        weatherData.innerHTML = `<p>Current-time: ${data.date}</p>
+            <p>Temperature in Celsius: ${data.temp}</p>
+            <p>Weather: ${data.description}</p>`;
+    })
+    .catch((error) => {
+      // console.error(error);
+    });
 });
