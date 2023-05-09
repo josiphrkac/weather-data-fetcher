47d98d77b30b8a93aada0fbd1071b72aa03d12fd
jhrkac <hrkacjosip0@gmail.com>
2023-05-09 13:07:09 +0200

    Finished PHP code to fetch weather data from OpenWeatherMap API and enabled send data to script.js as JSON.

--- api/catchData.php
+++ api/catchData.php
@@ -20,5 +20,21 @@ $response = curl_exec($curl);
 curl_close($curl);
 
 $data = json_decode($response, true);
-echo '<pre>';
-print_r($data);
+/* echo '<pre>';
+print_r($data); */
+
+if ($data['cod'] === 200) {
+    $weatherData = array(
+        'date' => date('d M', $data['dt']),
+        'temp' => round($data['main']['temp'] - 273.15),
+        'description' => $data['weather']['0']['description']
+    );
+    $json_result = json_encode($weatherData);
+    echo $json_result;
+} else {
+    $error = array(
+        'error' => 'An error occurred while fetching data'
+    );
+    $json_error = json_encode($error);
+    echo $json_error;
+}
